d753be3694d110775aa1a464226e4a2d4ae6edc4
My Name <kiykobogdan@gmail.com>
2023-01-27 22:45:57 +0200

    error

---
+++ Programa stud/Source.cpp
@@ -0,0 +1,29 @@
+#include "iostream"
+#include "string"
+#include "method.h"
+using namespace std;
+
+class TrainWord {
+
+public:
+	string WordOrgnl;
+	string WordTrslt;
+	void CreateObj();
+private:
+	//TrainWord(string WordOrgnl, string WordTrslt) {
+		//this->WordOrgnl = WordOrgnl;
+		//this->WordTrslt = WordTrslt;
+	//};
+	 
+
+};
+
+
+int main() {
+	TrainWord obj1;
+	obj1.CreateObj();
+
+
+};
+
+ 
---
+++ Programa stud/method.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Source.cpp"
+void TrainWord::CreateObj()
+{
+	cout << endl << "Input Word: ";
+	cin >> WordOrgnl;
+	cout << endl << "Input Word-Translate: ";
+	cin >> WordTrslt;
+};
